aa515a6af84b8ee2ca1c003722826f409109d91e
Lejla Omerovic <macbookpro@Lejlas-MacBook-Pro-2.local>
2023-03-26 12:59:55 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OpportunityService } from "./services/opportunity.service";
+import {OpportunityResolver} from "./resolvers/opportunity.resolver";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    OpportunityService,
+    OpportunityResolver
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
