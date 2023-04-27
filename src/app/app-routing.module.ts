f259e033a9ac5ad0a95165a2b7706f5911967724
Adela Joldic <adela.joldic@stu.ssst.edu.ba>
2023-04-27 14:32:40 +0200

    Fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ import {LandingComponent} from "./landing/landing.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {OrgRegisterComponent} from "./org-register/org-register.component";
+import {MyWorkComponent} from "./my-work/my-work.component";
+import {StudentListComponent} from "./student-list/student-list.component";
+import {MyWorkResolver} from "./resolvers/my-work.resolver";
 
 const routes: Routes = [
   {
@@ -24,12 +27,18 @@ const routes: Routes = [
   {
 
     path: "home",
-    component: HomeComponent
+    component: HomeComponent,
   },
-
+  {
+    path: "my-works",
+    component: MyWorkComponent,
+    resolve:{
+      'works' : MyWorkResolver
+    }
+  },
   {
     path: "register",
-    component: RegisterComponent
+    component: RegisterComponent,
   },
   {
     path: "opportunity/:id",
